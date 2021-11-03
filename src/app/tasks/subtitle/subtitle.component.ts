53898815dda8a907dae580b4467bf06538e26c56
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-02 23:24:13 -0500

    create: subtitle component

---
+++ src/app/tasks/subtitle/subtitle.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitle',
+  templateUrl: './subtitle.component.html',
+  styleUrls: ['./subtitle.component.css'],
+})
+export class SubtitleComponent {}
