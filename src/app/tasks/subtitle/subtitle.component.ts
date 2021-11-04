1fb3b75dd6a3abf03fd99dcd36b21296230fedcc
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-03 23:46:09 -0500

    add: mensaje base numero tareas

--- src/app/tasks/subtitle/subtitle.component.ts
+++ src/app/tasks/subtitle/subtitle.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import TasksService from '../services/tasks.service';
 
 @Component({
   selector: 'app-subtitle',
   templateUrl: './subtitle.component.html',
   styleUrls: ['./subtitle.component.css'],
 })
-export class SubtitleComponent {}
+export class SubtitleComponent {
+  constructor(private tasksServices: TasksService) {}
+
+  get numberTasks() {
+    return this.tasksServices.tasks.length;
+  }
+}
