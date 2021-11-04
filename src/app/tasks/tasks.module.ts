9e4665af6c944638942518d63bbe84fe8239082f
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-03 23:41:52 -0500

    update: layout modularizado

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubtitleComponent } from './subtitle/subtitle.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { GridComponent } from './grid/grid.component';
+import TasksService from './services/tasks.service';
 
 @NgModule({
-  declarations: [SubtitleComponent, TaskListComponent],
+  declarations: [SubtitleComponent, TaskListComponent, GridComponent],
   imports: [CommonModule],
-  exports: [SubtitleComponent, TaskListComponent],
+  exports: [GridComponent],
+  providers: [TasksService],
 })
 export class TasksModule {}
