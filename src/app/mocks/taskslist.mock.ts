ca2cca0e3fd2443dd8a98d20e03e5c8e276d616f
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-03 23:42:18 -0500

    create: mocks lista de tareas

---
+++ src/app/mocks/taskslist.mock.ts
@@ -0,0 +1,19 @@
+import { ITaskList } from '../tasks/interfaces/service.interfaces';
+
+export const mockTasksList: ITaskList[] = [
+  {
+    id: 1,
+    desc: 'sweeping',
+    completed: false,
+  },
+  {
+    id: 2,
+    desc: 'mopping',
+    completed: false,
+  },
+  {
+    id: 3,
+    desc: 'cooking',
+    completed: false,
+  },
+];
